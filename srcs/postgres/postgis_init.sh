6eadf577e7ab45ed7087584403d151f32e09850c
michael <michaelguerga@gmail.com>
2024-11-10 18:47:12 +0100

    now with sed scripting to get the right hosts credentials

--- srcs/postgres/postgis_init.sh
+++ srcs/postgres/postgis_init.sh
@@ -6,6 +6,12 @@ mkdir /var/lib/postgresql/data
 chmod 700 /var/lib/postgresql/data
 initdb -D /var/lib/postgresql/data
 
+sed -i '/.*replication.*all.*/d' /var/lib/postgresql/data/pg_hba.conf 
+sed -i '/^#.*/d' /var/lib/postgresql/data/pg_hba.conf 
+sed -i 's/127\.0\.0\.1\/32/172\.28\.0\.0\/16/' /var/lib/postgresql/data/pg_hba.conf
+sed -i '/^$/d' /var/lib/postgresql/data/pg_hba.conf
+sed -i 's/trust/scram-sha-256/' /var/lib/postgresql/data/pg_hba.conf
+
 pg_ctl start -D /var/lib/postgresql/data
 createuser $DB_USER
 createdb --encoding=UTF8 --owner=$DB_USER $DB_NAME
